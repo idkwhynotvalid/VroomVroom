ce4fe0a295f96f30eecf3a6f25ec0954a74ec477
idkwhynotvalid <149487522+idkwhynotvalid@users.noreply.github.com>
2023-11-27 15:19:08 +0100

    Update Textdokument (neu).py

--- Textdokument (neu).py
+++ Textdokument (neu).py
@@ -2,13 +2,13 @@ import pygame
 import random
 import sys
 import os
-<<<<<<< HEAD
+
 import time
 
-=======
+
 import time 
 from random import randrange 
->>>>>>> c3d3ffeca0564769ef9d587860a5a9a5f8551830
+
 # Initialize Pygame
 pygame.init()
 
@@ -71,15 +71,11 @@ while True:
             if angle > 0:
                 angle -= 2*rotation_speed
             elif angle < 0:
-<<<<<<< HEAD
-                angle += rotation_speed
-
-=======
                 angle += 2*rotation_speed
         
         
         
->>>>>>> c3d3ffeca0564769ef9d587860a5a9a5f8551830
+
         if keys[pygame.K_LEFT] and player_x >= 150:
             player_x -= 8
             angle += rotation_speed
