9d01ea5cb0edbc7defc0f3dc4e2621f2cacc1612
David-saj <david.sajosi@gmail.com>
2023-12-02 17:25:16 +0100

    Update Textdokument (neu).py

--- Textdokument (neu).py
+++ Textdokument (neu).py
@@ -2,6 +2,7 @@ import pygame
 import random
 import sys
 import os
+import math
 
 import time
 
@@ -34,10 +35,16 @@ circle_color_warning = (255, 0, 0)  # Red
 circle_spawn_interval = 10  # in seconds
 circle_warning_duration = 2  # in seconds
 
+
+def spawn_circle():
+    circle = Circle(player_x)
+    circles.append(circle)
+    return time.time()
+
 class Circle:
     def __init__(self, player_x):
         self.x = player_x
-        self.y = -circle_radius
+        self.y = HEIGHT - HEIGHT / 10
         self.color = (255, 255, 255)
         self.warning_start_time = None
         
@@ -61,8 +68,7 @@ enemy_car_img = pygame.transform.scale(enemy_car_img, (int(enemy_car_img.get_wid
 
 # Initialize player car position
 player_x = WIDTH // 2 - player_car_img.get_width() // 2
-player_y = HEIGHT - player_car_img.get_height() - 20
-
+player_y = HEIGHT - HEIGHT / 5
 # Initialize enemy cars
 enemy_cars = []
 #circles
@@ -71,7 +77,9 @@ circles = []
 # Initialize variables
 start_time = time.time()
 game_over = False
-
+last_circle_spawn_time = pygame.time.get_ticks()
+circle_follow_duration = 4  # in seconds
+circle_follow_start_time = None
 # Add variables to hold rotation angle and rotation speed
 angle = 0
 rotation_speed = 1
@@ -101,26 +109,42 @@ while True:
         if keys[pygame.K_RIGHT] and player_x + enemy_car_img.get_width() <= 750:
             player_x += 8
             angle -= rotation_speed
-        if keys[pygame.K_UP] and player_y - 30 >= 0:
+        if keys[pygame.K_UP] and player_y - HEIGHT / 10 >= 0:
             player_y -= 6
-        if keys[pygame.K_DOWN] and player_y + 30 + enemy_car_img.get_height() <= HEIGHT:
+        if keys[pygame.K_DOWN] and player_y + HEIGHT / 10 + enemy_car_img.get_height() <= HEIGHT:
             player_y += 6
 
     # Calculate elapsed time
     current_time = time.time() - start_time
     if int(current_time) % 1 == 0:
         score += 1/60
+        
+        
+        
+        
+  
+
     elapsed_time = time.time() - start_time
-    if elapsed_time % circle_spawn_interval < 0.1:
-        circles.append(Circle(player_x))
+    if elapsed_time - last_circle_spawn_time >= circle_spawn_interval:
+        last_circle_spawn_time = spawn_circle()
+        
 
 
-    # Move enemy cars and spawn new ones
+
+
+
+
+        # Move enemy cars and spawn new ones
     for car in enemy_cars:
         distance_to_bottom = HEIGHT - player_y
         car.y += current_time ** 0.5 + car.speed + 0.01 * distance_to_bottom
         if car.y > HEIGHT:
             enemy_cars.remove(car)
+
+
+    if circle_follow_start_time and time.time() - circle_follow_start_time > circle_follow_duration:
+        circles = []
+
 
 
     for lane in range(NUM_LANES):
@@ -139,20 +163,17 @@ while True:
     
     
     for circle in circles:
-        circle.y += current_time ** 0.5  # Adjust the movement speed as needed
-
-        # Check if the player collides with the circle
-        circle_rect = pygame.Rect(circle.x - circle_radius, circle.y - circle_radius, 2 * circle_radius, 2 * circle_radius)
-        if player_rect.colliderect(circle_rect):
-            if circle.color == circle_color_warning:
-                game_over = True
-            else:
-                circle.warning_start_time = time.time()
-                circle.color = circle_color_warning
-
+        circle_speed = 1  # Adjust the speed as needed
+        distance_x = player_x - circle.x
+        distance_y = player_y - circle.y
+        angle1 = math.atan2(distance_y, distance_x)
+        circle.x += circle_speed * math.cos(angle1) 
+        circle.y += circle_speed * math.sin(angle1) 
+        
         # Remove circles that go off-screen or have exceeded the warning duration
-        if circle.y > HEIGHT or (circle.warning_start_time and time.time() - circle.warning_start_time > circle_warning_duration):
+        if circle.warning_start_time and time.time() - circle.warning_start_time > circle_warning_duration:
             circles.remove(circle)
+
     
     
     
